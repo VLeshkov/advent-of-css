7e72304361868d8307553c239c37413f858649e0
Victor Leshkov <7065531+VLeshkov@users.noreply.github.com>
2021-12-09 10:20:12 +0600

    feat: add simple backward 15min timer

--- pomodoro-timer/src/index.js
+++ pomodoro-timer/src/index.js
@@ -1,24 +1,30 @@
 let startStopButton = document.querySelector('.start-stop-button');
 let isTimerRunning = false;
 
-/*
-let currentTime = new Date();
 
-currentTime = Date.parse(currentTime);
+let currentTime = 900;
 
-let timerText = document.getElementById('timer-text');
+let timerInterval;
 
+function writeTime() {
+  document.getElementById('timer-text').textContent = `${('0' + String(Math.trunc(currentTime / 60))).substr(-2)}:${('0' + String(currentTime % 60)).substr(-2)}`
 
-timerText.textContent =  currentTime;
-*/
+  if (currentTime <= 0) {
+    currentTime = 900
+  } else {
+    currentTime -= 1;
+  }
+}
+
 
 startStopButton.addEventListener('click', function() {
   if (isTimerRunning == false) {
     startStopButton.textContent = 'STOP';
     isTimerRunning = true;
-    currentTime.getSeconds() += 1;
+    timerInterval = setInterval(writeTime, 1000);
   } else {
     startStopButton.textContent = 'START';
     isTimerRunning = false;
+    clearInterval(timerInterval);
   };
 });
